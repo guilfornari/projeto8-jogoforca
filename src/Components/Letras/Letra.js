a3da86a7cf2a21f2c15b1dc9e38f58e2cd428073
Guilfornari <guilfornari@gmail.com>
2023-02-25 14:12:23 -0300

    Failed to correct codesmell accusing four instances of check

--- src/Components/Letras/Letra.js
+++ src/Components/Letras/Letra.js
@@ -1,17 +1,17 @@
 export default function Letra({ chosenLetters, buttonFunction, letter, guessedLetters, errorCounter }) {
+
     const deadByHanging = 6;
-    function toggleDisable(chosenLetters, guessedLetters, errorCounter) {
-        if (chosenLetters.length > 0 &&
-            !guessedLetters.includes(letter) &&
-            chosenLetters.includes("_") &&
-            errorCounter !== deadByHanging) {
-            return true;
-        } else { return false };
-    }
+
     return (
         <button data-test="letter" onClick={() => buttonFunction(letter)}
-            disabled={(toggleDisable) ? false : true}
-            className={(toggleDisable) ? "enabled-button" : undefined}>
+            disabled={(chosenLetters.length > 0 &&
+                !guessedLetters.includes(letter) &&
+                chosenLetters.includes("_") &&
+                errorCounter !== deadByHanging) ? false : true}
+            className={(chosenLetters.length > 0 &&
+                !guessedLetters.includes(letter) &&
+                chosenLetters.includes("_") &&
+                errorCounter !== deadByHanging) ? "enabled-button" : undefined}>
             {letter}
         </button>
     );
